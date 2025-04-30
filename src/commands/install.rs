a6f42af67649b2bac237360f8dae3a537c1875a0
Li-Lun Lin <70696274+alan910127@users.noreply.github.com>
2025-04-30 12:33:40 +0800

    feat: install WasmEdge to specified directory (#26)
    * feat: install WasmEdge to specified directory
    
    Signed-off-by: alan910127 <70696274+alan910127@users.noreply.github.com>
    
    * feat: use bin layout support `-v` flags
    
    Signed-off-by: alan910127 <70696274+alan910127@users.noreply.github.com>
    
    * fix: windows build
    
    Signed-off-by: alan910127 <70696274+alan910127@users.noreply.github.com>
    
    * refactor: separate responsibilities
    
    Signed-off-by: alan910127 <70696274+alan910127@users.noreply.github.com>
    
    * test: add install command integration test
    
    Signed-off-by: alan910127 <70696274+alan910127@users.noreply.github.com>
    
    * fix: remove example to avoid doc-testing extract_archive
    
    Signed-off-by: alan910127 <70696274+alan910127@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: alan910127 <70696274+alan910127@users.noreply.github.com>

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -1,8 +1,24 @@
 use std::path::PathBuf;
 
 use clap::Parser;
+use semver::Version;
+use snafu::ResultExt;
 
-use crate::target::{TargetArch, TargetOS};
+use crate::{
+    api::{Asset, WasmEdgeApiClient},
+    cli::{CommandContext, CommandExecutor},
+    prelude::*,
+    target::{TargetArch, TargetOS},
+};
+
+fn default_path() -> PathBuf {
+    let home_dir = dirs::home_dir().expect("home_dir should be present");
+    home_dir.join(".wasmedge")
+}
+
+fn default_tmpdir() -> PathBuf {
+    std::env::temp_dir()
+}
 
 #[derive(Debug, Parser)]
 pub struct InstallArgs {
@@ -17,7 +33,7 @@ pub struct InstallArgs {
 
     /// Set the temporary directory for staging downloaded assets
     ///
-    /// Defaults to `/tmp` on Unix-like systems and `%LOCALAPPDATA%\Temp` on Windows.
+    /// Defaults to the system temporary directory, this differs between operating systems.
     #[arg(short, long)]
     pub tmpdir: Option<PathBuf>,
 
@@ -32,4 +48,75 @@ pub struct InstallArgs {
     /// `wasmedgeup` will detect the architecture of your host system by default.
     #[arg(short, long)]
     pub arch: Option<TargetArch>,
+}
+
+impl CommandExecutor for InstallArgs {
+    /// Executes the installation process by resolving the version, downloading the asset,
+    /// unpacking it, and copying the extracted files to the target directory.
+    ///
+    /// # Steps:
+    /// 1. Resolves the version (either a specific version or the latest).
+    /// 2. Downloads the asset for the appropriate OS and architecture.
+    /// 3. Unpacks the asset to a temporary directory.
+    /// 4. Copies the extracted files to the target directory.
+    ///
+    /// # Arguments
+    ///
+    /// * `ctx` - The command context containing the client and progress bar settings.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if any step fails, such as download failure, extraction issues,
+    /// or copying issues.
+    #[tracing::instrument(name = "install", skip_all, fields(version = self.version))]
+    async fn execute(mut self, ctx: CommandContext) -> Result<()> {
+        let version = self.resolve_version(&ctx.client).inspect_err(
+            |e| tracing::error!(error = %e.to_string(), "Failed to resolve version"),
+        )?;
+        tracing::debug!(%version, "Resolved version for installation");
+
+        let os = self.os.get_or_insert_default();
+        let arch = self.arch.get_or_insert_default();
+        tracing::debug!(?os, ?arch, "Host OS and architecture detected");
+
+        let asset = Asset::new(&version, os, arch);
+        let tmpdir = self.tmpdir.unwrap_or_else(default_tmpdir);
+        let file = ctx
+            .client
+            .download_asset(&asset, &tmpdir, ctx.no_progress)
+            .await
+            .inspect_err(|e| tracing::error!(error = %e.to_string(), "Failed to download asset"))?;
+
+        tracing::debug!(file_path = %file.path().display(), dest = %tmpdir.display(), "Starting extraction of asset");
+        crate::fs::extract_archive(file.into_file(), &tmpdir)
+            .await
+            .inspect_err(|e| tracing::error!(error = %e.to_string(), "Failed to extract asset"))?;
+        tracing::debug!(dest = %tmpdir.display(), "Extraction completed successfully");
+
+        // Try with `tmpdir/WasmEdge-{version}-{os}` first, and if it's not a directory, fallback
+        // to `tmpdir`
+        // (both patterns are used in WasmEdge)
+        let mut extracted_dir = tmpdir.join(&asset.install_name);
+        if !extracted_dir.is_dir() {
+            tracing::debug!(extracted_dir = %extracted_dir.display(), "Falling back to tmpdir as extracted directory");
+            extracted_dir = tmpdir;
+        }
+
+        let target_dir = self.path.unwrap_or_else(default_path);
+        tracing::debug!(extracted_dir = %extracted_dir.display(), target_dir = %target_dir.display(), "Start copying files to target location");
+        crate::fs::copy_tree(&extracted_dir, &target_dir).await;
+        tracing::debug!(target_dir = %target_dir.display(), "Copying files to target location completed");
+
+        Ok(())
+    }
+}
+
+impl InstallArgs {
+    fn resolve_version(&self, client: &WasmEdgeApiClient) -> Result<Version> {
+        if self.version == "latest" {
+            client.latest_release()
+        } else {
+            Version::parse(&self.version).context(SemVerSnafu {})
+        }
+    }
 }
---
+++ src/fs.rs
@@ -0,0 +1,107 @@
+use crate::prelude::*;
+use snafu::ResultExt;
+
+use std::path::{Path, PathBuf};
+
+use tokio::fs;
+use walkdir::WalkDir;
+
+pub async fn copy_tree(from_dir: &Path, to_dir: &Path) {
+    let num_components = from_dir.components().count();
+
+    for entry in WalkDir::new(from_dir).into_iter().filter_map(|e| e.ok()) {
+        tracing::trace!(entry = %entry.path().display(), "Copying entry");
+        let Ok(metadata) = entry.metadata() else {
+            continue;
+        };
+        if !metadata.is_file() {
+            continue;
+        }
+
+        // Calculate the target location based on from_dir, to_dir, and entry
+        // by first calculate the path of entry relative to from_dir, and then append it to to_dir
+        //
+        // # Example
+        // from_dir = '/from/path
+        // entry = '/from/path/foo/bar/something.txt'
+        // to_dir = '/to/path'
+        // => num_components = 3 ([RootDir, "from", "path"])
+        // => chained = [RootDir, "to", "path"].chain(["foo", "bar", "something.txt"])
+        // => target_loc = "/to/path/foo/bar/something.txt"
+        let target_loc = to_dir
+            .components()
+            .chain(entry.path().components().skip(num_components))
+            .collect::<PathBuf>();
+
+        let Some(parent) = target_loc.parent() else {
+            tracing::warn!(location = %target_loc.display(), "Missing parent for target location");
+            continue;
+        };
+        if let Err(e) = fs::create_dir_all(parent).await {
+            tracing::warn!(error = %e, directories = %parent.display(), "Failed to create directories");
+            continue;
+        };
+
+        if let Err(e) = fs::copy(entry.path(), &target_loc).await {
+            tracing::warn!(
+                error = %e,
+                entry = %entry.path().display(),
+                target_loc = %target_loc.display(),
+                "Failed to copy file to target location",
+            );
+        };
+    }
+}
+
+/// Extracts the contents of a compressed archive (`.tar.gz` for Unix-like systems, `.zip` for Windows) to a specified directory.
+///
+/// # Arguments
+///
+/// * `file` - A file object representing the compressed archive. This file must be opened in read mode.
+/// * `dest` - The destination directory to which the contents will be extracted.
+///
+/// # Errors
+///
+/// Returns an error if the extraction fails. This could happen if the archive format is unsupported or
+/// if the destination path cannot be created.
+pub async fn extract_archive(mut file: std::fs::File, dest: &Path) -> Result<()> {
+    use std::io::Seek;
+    use tokio::fs;
+
+    fs::create_dir_all(dest).await.inspect_err(
+        |e| tracing::error!(error = %e.to_string(), "Failed to create directory during extraction"),
+    )?;
+    file.rewind()?;
+
+    #[cfg(unix)]
+    {
+        use flate2::read::GzDecoder;
+        let decompressed = GzDecoder::new(file);
+        extract_tar(decompressed, dest)?;
+    }
+
+    #[cfg(windows)]
+    extract_zip(file, dest)?;
+
+    Ok(())
+}
+
+#[cfg(unix)]
+fn extract_tar(file: impl std::io::Read, to: &Path) -> Result<()> {
+    use tar::Archive;
+
+    let mut archive = Archive::new(file);
+    archive.unpack(to).context(ExtractSnafu {})?;
+
+    Ok(())
+}
+
+#[cfg(windows)]
+fn extract_zip(file: std::fs::File, to: &Path) -> Result<()> {
+    use zip::ZipArchive;
+
+    let mut archive = ZipArchive::new(file).context(ExtractSnafu {})?;
+    archive.extract(to).context(ExtractSnafu {})?;
+
+    Ok(())
+}
